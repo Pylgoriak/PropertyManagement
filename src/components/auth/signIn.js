603781daffc318fe1f52d692b8da0e9be1887d99
Jared Grant <jared.grant@canyonsdistrict.org>
2018-07-23 10:35:18 -0600

    formed title component

--- src/components/auth/signIn.js
+++ src/components/auth/signIn.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import { FormTitle } from '../formTitle';
 
 class SignIn extends Component {
     render() {
         return (
             <div className="signIn">
-                Sign In
+                <FormTitle className="signIn__title" text="Sign in" />
             </div>
         )
     }
