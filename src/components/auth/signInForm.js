d71802cf4c942f1a30ccfde41d19658d448c9469
Jared Grant <jared.grant@canyonsdistrict.org>
2018-07-25 08:48:18 -0600

    styled inputs in form fields scss file

--- src/components/auth/signInForm.js
+++ src/components/auth/signInForm.js
@@ -9,7 +9,24 @@ class SignInForm extends Component {
         return (
             <form className="signInForm">
                 <FormTitle className="signIn-form__title" text="Sign in" />
-                <Field className="signIn-form__email"component={FormInput} name="email" type="email"/>
+
+                <Field
+                    className="signIn-form__email"
+                    title="Email"
+                    placeholder="Enter Email"
+                    name="email"
+                    type="email"
+                    component={FormInput}
+                />
+
+                <Field
+                    className="signIn-form__password"
+                    title="Password"
+                    placeholder="Enter Password"
+                    name="password"
+                    type="password"
+                    component={FormInput}
+                />
             </form>
         )
     }
--- src/components/formFields.js
+++ src/components/formFields.js
@@ -1,12 +1,17 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 export class FormInput extends Component {
     render() {
-        const { className, title } = this.props;
+        const { className, title, input, type, placeholder } = this.props;
         return (
-            <div className="formInput">
-                <label>{title}</label>
-                <input className={`${className} formInput`}/>
+            <div className='form-input'>
+                <label className='form-input__title'>{title}</label>
+                <input
+                    className={`${className} form-input__input`}
+                    type={type}
+                    {...input}
+                    placeholder={placeholder}    
+                />
             </div>
         )
     }
